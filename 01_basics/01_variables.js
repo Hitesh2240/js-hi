654dc964706c26e0e195f29052dae7ec86faad52
Hitesh Kumar Sharma <115990443+Hitesh2240@users.noreply.github.com>
2023-11-01 06:40:38 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const accountId = 1223
+let accountEmail = "hitesh@gmail.com"
+var accountPassword = "Jaipur"
+
+// accountId = 2
+console.log(accountId);
+console.table([accountEmail, accountId, accountPassword])
