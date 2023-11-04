72e076f56776daeebf4057a87b87204443d4e6ea
Hitesh Kumar Sharma <115990443+Hitesh2240@users.noreply.github.com>
2023-11-04 02:55:15 +0000

    dataType-summary

---
+++ 01_basics/dataType-summary.js
@@ -0,0 +1,30 @@
+//primitive 
+
+//7 type => string, number, boolean, null, undefined, symbol, BogInt
+
+const score = 100 
+const scoreVlaue = 100.3
+
+
+const outSideTemp = null 
+let userEmail;
+const Id = Symbol("123")
+const anotherId = Symbol("123")
+
+console.log(Id === anotherId)
+
+const bigNumber = 2345678901234556567n
+
+//Refernce (Non primitive)
+
+// Array, Objects, Function 
+const heros = ["shaktiman", "naagraj", "doga"]; // array
+
+let myObj = {
+    name: "Hitesh"
+    age: 22;
+} // object
+
+const myFunction = function(){
+    console.log("Hello world")
+} 
