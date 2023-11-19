92f1fc6c40e22613581f2d3ca2e1d7321ddfab5f
Hitesh Kumar Sharma <115990443+Hitesh2240@users.noreply.github.com>
2023-11-19 11:49:48 +0000

    function

---
+++ 03_basics/01_function.js
@@ -0,0 +1,25 @@
+// function sayName(){
+//     console.log("H")
+//     console.log("I")
+//     console.log("T")
+//     console.log("E")
+//     console.log("S")
+//     console.log("H")
+// }
+// // sayName this is refernce of the function
+// //sayName() this is mean call the function
+// sayName()
+//those putting the value with function like upper one (number1, number2) are called parameter and when call the function, giving wwith call is called argument 
+// function addTwoNumber (number1, number2){
+//     console.log(number1 + number2)
+// }
+function addTwoNumber (number1, number2){
+    let result = number1 + number2
+    return result //retuen value only store in another variable and cannot store console value in another variable 
+}
+const result = addTwoNumber(3,4)
+
+// console.log("Result", result);
+
+
+
