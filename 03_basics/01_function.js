9bdd2a1199d0ed4db8468ea76616087441cc2fee
Hitesh Kumar Sharma <115990443+Hitesh2240@users.noreply.github.com>
2023-11-20 06:00:40 +0000

    function

--- 03_basics/01_function.js
+++ 03_basics/01_function.js
@@ -20,6 +20,42 @@ function addTwoNumber (number1, number2){
 const result = addTwoNumber(3,4)
 
 // console.log("Result", result);
+function loginUserMessage(username = "sam"){
+    if(!username){
+        console.log("Please enter a username");
+    }
+    return `${username} just logged in`
+}
+// console.log(loginUserMessage(""))
+function calculateCartPrice(val1, val2,...num1){ // ... in function is konwn rest operator not spread operator
+    return num1
+}
+// console.log(calculateCartPrice(200, 400, 400, 2000))
 
+// ************************How to get the object in the function
+const user = {
+    username: "Hitesh",
+    price: 199
+}
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`)
+}
+// handleObject(user)
+// handleObject({
+//     username: "Sam",
+//     price: 399
+// })
+
+
+
+// **************************************how to get array in function*******************************
+
+const myNewArray = [200, 300, 400, 500]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 500, 800, 1000]))
 
 
