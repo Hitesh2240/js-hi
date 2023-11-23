51cd3de1069d3f84aa4d8e61be49cd31ac67fd13
Hitesh Kumar Sharma <115990443+Hitesh2240@users.noreply.github.com>
2023-11-23 17:58:54 +0000

    iife

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,13 @@
+//Immediately Invoked Function Expression (IIFE)
+
+(function chai (){
+    //name IIFE
+    console.log('DB CONNECTED');
+
+})
+();
+
+(()=>{
+    //only IIFE
+    console.log('Hello')
+})()
