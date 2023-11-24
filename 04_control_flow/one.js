3875e97862734a97dd0b61103a1eb9272fc78445
Hitesh Kumar Sharma <115990443+Hitesh2240@users.noreply.github.com>
2023-11-24 06:30:21 +0000

    if else

---
+++ 04_control_flow/one.js
@@ -0,0 +1,18 @@
+const temperature = 41
+// if(temperature== 40){
+//     console.log("less than 50")
+// }else{
+//     console.log("temperature is less than 50 ")
+// }
+// console.log("executive")
+
+// const score = 200
+
+// if(score>100){
+//     const power = "fly"
+//     console.log(`User power: ${power}`)
+// }
+// console.log(`User power: ${power}`)
+
+const balance = 1000;
+if(balance>500) console.log("test");
