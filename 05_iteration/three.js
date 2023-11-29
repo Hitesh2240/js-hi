ec4c448848c63b123e5f06fb0e994d1501c96406
Hitesh Kumar Sharma <115990443+Hitesh2240@users.noreply.github.com>
2023-11-29 09:15:50 +0000

    For of and map

--- 05_iteration/three.js
+++ 05_iteration/three.js
@@ -2,6 +2,23 @@
 const arr = [1,2,3,4,5]
 
 for (const num of arr) {
-    console.log(num)
+    // console.log(num)
+    
+}
+const greeting = "Hello World"
+for (const greet of greeting) {
+    // console.log(`Each char is ${greet}`)
+}
+
+// Maps
+
+const map = new Map()
+map.set('IN', "India")
+map.set('USA', "United states of America")
+map.set('FR', "France")
+
+// console.log(map)
+for (const [key, value] of map) {
+    console.log(key, ':-', value)
     
 }
