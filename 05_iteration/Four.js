26a5a8d3c9394f5a8466a4d0b909a0a0ea44e807
Hitesh Kumar Sharma <115990443+Hitesh2240@users.noreply.github.com>
2023-11-29 09:37:36 +0000

    Forin loop

---
+++ 05_iteration/Four.js
@@ -0,0 +1,16 @@
+const myObject = {
+    js: "Javascript",
+    cpp: "C++",
+    rb: "Ruby",
+    swift : "swift by apple"
+}
+// forin
+for (const key in myObject) {
+//    console.log(`${key}  shortcut is for ${myObject[key]}`);
+}
+
+const programing = ["jss", "rb", "Py", "Java", "cpp"]
+
+for (const key in programing) {
+   console.log(programing[key])
+}
